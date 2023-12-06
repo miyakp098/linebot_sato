4db7062fd1dabdd9b38e4a8688b2a48670fcd784
miyakp098 <ybtkeppen813@gmail.com>
2023-12-06 15:39:46 +0900

    test

--- index.js
+++ index.js
@@ -18,19 +18,29 @@ app.post('/', middleware(config), (req, res) => {
       const userId = event.source.userId;
 
       if (userStates[userId] === 'waiting_for_reply') {
-        // 10秒後におうむ返し
-        setTimeout(() => {
-            userStates[userId] = 'waiting_for_reply';
-          }, 10000);
+        // おうむ返しの状態であれば、メッセージをおうむ返しして状態をリセット
+        userStates[userId] = 'normal'; // 状態をリセット
+        return client.replyMessage(event.replyToken, {
+          type: 'text',
+          text: event.message.text // ユーザーのメッセージをそのまま返す
+        });
       }
 
       if (userStates[userId] === 'waiting_for_yes') {
         // 「はい」と答えた場合の処理
         if (event.message.text === 'はい') {
           userStates[userId] = 'waiting_for_reply';
-        }else if(event.message.text === 'いいえ') {
+
+          // 10秒後におうむ返し状態をリセット
+          setTimeout(() => {
             userStates[userId] = 'normal';
+          }, 10000);
+
+          return Promise.resolve(null);
         }
+
+        // 「いいえ」と答えた場合、またはそれ以外の場合は状態をリセット
+        userStates[userId] = 'normal';
       }
 
       // 初期状態または状態リセット後の処理
